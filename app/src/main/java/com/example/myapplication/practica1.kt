ab6c7250483395856fcec4459e69e52064a0a27a
lesrac23 <lesrac23@gmail.com>
2024-07-10 11:37:10 -0600

    Mi primer commit

---
+++ app/src/main/java/com/example/myapplication/practica1.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication
+
+fun main(){
+    val naranjas = 20
+    val manzanas = 45
+    val fresas = 60
+    val tunas = 30
+
+    val suma= (naranjas + manzanas + fresas + tunas)
+    // este es un comentario
+
+    println("El precio de las frutas es: $suma" )
+
+
+}
