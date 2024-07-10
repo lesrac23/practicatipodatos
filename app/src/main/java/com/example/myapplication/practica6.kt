6e2da3561a9dfbdc50633a8df4a745bd132cc713
lesrac23 <lesrac23@gmail.com>
2024-07-10 16:11:57 -0600

    Tercer commit

---
+++ app/src/main/java/com/example/myapplication/practica6.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication
+
+fun main(parametro: Array<String>){
+    print("Ingresa el sueldo del empleado:")
+    val sueldo = readln().toDouble()
+    if(sueldo > 3000){
+        println("Debe pagar impuestos")
+    }
+
+}
